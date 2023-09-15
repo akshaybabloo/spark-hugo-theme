a6c05f6987d59eeaaa7ed97c65afb5310959f686
Akshay Raj Gollahalli <akshay.gollahalli@fisherpaykel.com>
2023-09-15 20:50:22 +1200

    update types

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,12 @@
 import { Icon } from "@fortawesome/fontawesome-svg-core";
 
-export function groupBy(array: Array<any>, key: string) {
+export function groupBy(array: Array<any>, key: string): { [key: string]: any[] } {
     return array.reduce((result, currentItem) => {
         (result[currentItem[key]] = result[currentItem[key]] || []).push(currentItem);
         return result;
     }, {});
 }
 
-export function getIconHtml(icon: Icon) {
-	return icon.html.pop();
+export function getIconHtml(icon: Icon): string | undefined {
+	return icon.html[0];
 }
